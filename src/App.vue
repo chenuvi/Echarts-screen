dbe2780f4fe177e0bbafd0a082f9996bccc6320e
chenuvi <chenuvi@outlook.com>
2023-11-20 15:46:08 +0800

    Feat: 组件初步布局

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import HorizontalBar from "./components/HorizontalBar.vue";
+import MapChart from "./components/MapChart.vue";
+import RadarBar from "./components/RadarBar.vue";
+import Relation from "./components/Relation.vue";
+import RingBar from "./components/RingBar.vue";
+import TotalData from "./components/TotalData.vue";
+import VerticalBar from "./components/VerticalBar.vue";
+import WordCloud from "./components/WordCloud.vue";
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline text-red-900">Hello world!</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div
+      class="bg-[url('assets/images/bg.png')] bg-cover bg-center h-screen text-white p-3 flex overflow-hiddens"
+    >
+      <!-- left -->
+      <div class="flex-1 mr-3 bg-opacity-50 bg-slate-800 p-3">
+        <!-- 横向柱状图 -->
+        <HorizontalBar class="h-1/3 box-border pb-2" />
+        <!-- 雷达图 -->
+        <RadarBar class="h-1/3 box-border pb-2" />
+        <!-- 关系图 -->
+        <Relation class="h-1/3" />
+      </div>
+      <!-- center -->
+      <div class="w-1/2 mr-3 flex flex-col">
+        <!-- 数据总览图 -->
+        <TotalData class="bg-opacity-50 bg-slate-800 p-3" />
+        <!-- 地图可视化 -->
+        <MapChart class="bg-opacity-50 bg-slate-800 p-3 mt-3 flex-1" />
+      </div>
+      <!-- right -->
+      <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
+        <!-- 竖向柱状图 -->
+        <VerticalBar class="h-1/3 box-border pb-2" />
+        <!-- 环形图 -->
+        <RingBar class="h-1/3 box-border pb-2" />
+        <!-- 文档云图 -->
+        <WordCloud class="h-1/3" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/RadarBar.vue
@@ -0,0 +1,10 @@
+<!--  -->
+<script setup lang="ts">
+import {} from "vue";
+</script>
+
+<template>
+  <div class=""></div>
+</template>
+
+<style lang="less" scoped></style>
