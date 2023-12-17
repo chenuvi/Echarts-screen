4120165828541b401f2c0ca59ba636b080e139f7
ChenUvi <chenuvi@outlook.com>
2023-12-18 01:00:45 +0800

    Feat: 静态资源

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
 
@@ -13,9 +11,6 @@ function App() {
           count is {count}
         </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
