3979192e4e3fc80da19149580052eaac42d9c943
ChenUvi <chenuvi@outlook.com>
2023-12-18 00:21:35 +0800

    Init and set tailwind

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className=" text-red-600 text-4xl">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
